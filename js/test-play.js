0cc1565d75271de53d85dca70c84ff0c4a0c5e30
natalie-macpherson <macphersonnatalie@gmail.com>
2015-01-26 19:15:04 -0500

    delete background-size: cover on .billboard

--- js/test-play.js
+++ js/test-play.js
@@ -1,15 +1,20 @@
-var pass =5;
-var score = 2;
+var question1 = function (){
 
-var hasPassed = score >= pass;
+	var pass =5;
+	var score = 2;
 
-var el = document.getElementById('answer');
-if (hasPassed)
-{
-	el.innerHTML = 'Level Passed: 1';
-}
-else
-{
-	el.innerHTML = 'Level Failed: You Suck';
-}
+	var hasPassed = score >= pass;
 
+	var el = document.getElementById('answer');
+	if (hasPassed)
+	{
+		el.innerHTML = 'Level Passed: 1';
+	}
+	else
+	{
+		el.innerHTML = 'Level Failed: You Suck';
+	}
+
+};
+
+document.querySelector('.button').addEventListener('click', question1);
