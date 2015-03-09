11a2b40f0573e724d7b748c1611d20949be453fa
natalie-macpherson <macphersonnatalie@gmail.com>
2015-03-08 23:04:45 -0400

    made tabs and started to type the look and the animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,27 @@
 var $btnMenu = $('.btn-menu');
 var $panel = $('.panel');
+var $btn = $('.btn');
+var $tabs = $('.tabs');
+var $panels = $('.panels');
 
 $btnMenu.on('click', function () {
 	$panel.toggleClass('js-panel-open');
 })
 
-$panel.on('animationend', function(){
-	$panel.removeClass('js-panel-open');
+//$panel.on('animationend', function(){
+	//$panel.removeClass('js-panel-open');
+//});
+
+
+
+
+$tabs.on('click', 'a', function (e) {
+  e.preventDefault();
+  var id = $(this).attr('href');
+
+  $panels.filter(':not([hidden])').attr('hidden', true);
+  $(id).removeAttr('hidden');
+
+  $tabs.find('.js-current').removeClass('js-current');
+  $(this).addClass('js-current');
 });
