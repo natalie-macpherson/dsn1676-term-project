70e912ce7d755c2c9bb872cb4dd2890dc62ea98f
natalie-macpherson <macphersonnatalie@gmail.com>
2015-03-09 20:44:58 -0400

    added a close btw for shadowbox and an animation

--- js/main.js
+++ js/main.js
@@ -1,23 +1,14 @@
 var $btnMenu = $('.btn-menu');
 var $panel = $('.panel');
 var $btn = $('.btn');
-//var $tabs = $('.tabs');
-//var $panels = $('.panels');
-
-$btnMenu.on('click', function () {
-	$panel.toggleClass('js-panel-open');
-});
-
-//$panel.on('animationend', function(){
-	//$panel.removeClass('js-panel-open');
-//});
-
-
 var $thumbs = $('.thumbnails');
 var $lightboxImg = $('.lightbox-img');
 var $lb = $('.lightbox');
 var $btnClose = $('.btn-close');
 
+$btnMenu.on('click', function () {
+	$panel.toggleClass('js-panel-open');
+});
 
 $thumbs.on('click', 'a', function(e){
 	e.preventDefault();
@@ -30,15 +21,4 @@ $btnClose.on('click', function() {
  $lb.attr('data-state', 'hidden');
 });
 
-//$tabs.on('click', 'a', function (e) {
-  //e.preventDefault();
-  //var id = $(this).attr('href');
 
-  //console.log ($panels.filter(':not([hidden])'));
-
-  //$panels.filter(':not([hidden])').attr('hidden', true);
-  //$(id).removeAttr('hidden');
-
-  //$tabs.find('.js-current').removeClass('js-current');
-  //$(this).addClass('js-current');
-//});
